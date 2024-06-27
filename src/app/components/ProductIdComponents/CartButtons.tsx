3c57b80eb04d3d8ff93d0379b65559bcbad82338
LucilaTineo <lucilamtineo@gmail.com>
2024-06-27 15:14:42 -0300

    nuevos cambios en 'feature/productDetail'

--- src/app/components/ProductIdComponents/CartButtons.tsx
+++ src/app/components/ProductIdComponents/CartButtons.tsx
@@ -1,10 +1,10 @@
-export default function CartButtons ({quantity, onIncrease, onDecrease} : {quantity:any, onIncrease: any, onDecrease:any}) {
+export default function CartButtons ({quantity, onIncrease, onDecrease, addToCart} : {quantity:any, onIncrease: any, onDecrease:any, addToCart:any}) {
   return(
       <div className="flex items-center justify-between">
           <button onClick= {onDecrease} className="bg-red-500 rounded-full p-2 ">-</button>
           <span className="text-lg font-semibold">{quantity}</span>
           <button onClick= {onIncrease} className="bg-red-500 rounded-full p-2 ">+</button>
-          <button className="bg-red-500 text-white rounded-lg px-4 py-2">Añadir al carrito</button>
+          <button onClick= {addToCart} className="bg-red-500 text-white rounded-lg px-4 py-2">Añadir al carrito</button>
       </div>
   );
 }
