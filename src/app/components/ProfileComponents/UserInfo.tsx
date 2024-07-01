108d00ed856c37187ce30ead9b383e069947ec6a
LucilaTineo <lucilamtineo@gmail.com>
2024-07-01 14:42:02 -0300

    Actualizar logica User dashboard

--- src/app/components/ProfileComponents/UserInfo.tsx
+++ src/app/components/ProfileComponents/UserInfo.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import DataField from './DataField';
 
-export interface IUser {
-  name: string;
-  email: string;
+type Role = 'user' | 'admin' | 'superAdmin';
+
+interface Credential {
+  id: string;
+  NIN: string;
   phone: string;
-  address: string;
-  password: string;
+}
+
+interface User {
+  id: string;
+  role: Role;
+  name: string;
+  NIN: string;
+  email: string;
+  googleAuth: boolean;
+  phone: string;
+  creditCardNumber: string;
+  virtualWallet: string;
+  preferredPaymentMethod: string;
+  deletedAt: string | null;
+  credential: Credential;
 }
 
 
-const UserInfo = ({user} : {user: IUser | null}) => {
+const UserInfo = ({user} : {user: User | null}) => {
 
   if(!user) {
     return (
@@ -23,15 +38,13 @@ const UserInfo = ({user} : {user: IUser | null}) => {
 
 
   return (
-    <div className="bg-white rounded-lg shadow-lg p-6 h-screen w-screen flex flex-col justify-center items-center space-y-4">
-      <header className="flex items-center justify-center w-full max-w-md mb-4">
+    <div className="bg-white rounded-lg shadow-lg p-6 w-full mx-auto mt-4 flex flex-col justify-center items-center space-y-4">
+      <header className="flex items-center justify-center w-full mb-4">
         <h1 className="text-2xl font-bold text-red-500"> Mi Cuenta</h1>
       </header>
       <DataField label = "Nombre" value= {user.name} editable = {false} />
-      <DataField label = "Email" value= {user.email} editable = {true} />
-      <DataField label = "Teléfono" value= {user.phone} editable = {true} />
-      <DataField label = "Dirección" value= {user.address} editable = {false} />
-      <DataField label = "Contraseña" value= {user.password} editable = {true} type = "password"/>
+      <DataField label = "Email" value= {user.email} editable = {false} />
+      <DataField label = "Teléfono" value= {user.phone} editable = {false} />
     </div>
   );
 };
