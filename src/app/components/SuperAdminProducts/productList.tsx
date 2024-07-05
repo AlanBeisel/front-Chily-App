095e48e1bbbeb0c176caf1562d5b5974e0a2426d
LucilaTineo <lucilamtineo@gmail.com>
2024-07-05 04:36:30 -0300

    Nuevos cambios feature/SuperAdminProducts

---
+++ src/app/components/SuperAdminProducts/productList.tsx
@@ -0,0 +1,143 @@
+'use client'
+import React, { useEffect, useState } from "react";
+import { fetchProducts, deleteProduct } from "@/helpers/peticionesSuperAdmin";
+import ConfirmModal from "./confirmModal";
+import { Product } from "@/types";
+import Link from "next/link";
+
+const PAGE_SIZE = 10;
+
+const ProductList: React.FC = ()=> {
+  const[products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [productToDelete, setProductToDelete] = useState<string | null>(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  useEffect(() =>{
+    fetchData();
+  },[currentPage]);
+
+
+    const fetchData = async () => {
+      try{
+      setLoading(true);
+      const startIndex = (currentPage -1) * PAGE_SIZE;
+      const endIndex = startIndex + PAGE_SIZE;
+      const productsData = await fetchProducts();
+      const slicedProducts = productsData.slice(startIndex, endIndex);
+      setProducts(slicedProducts);
+      setTotalPages(Math.ceil(productsData.length / PAGE_SIZE));
+    } catch (error) {
+      console.error('Error al obtener los productos', error);
+    } finally {
+      setLoading(false);
+    }
+    };
+
+  const handlePageChange = (page:number) => {
+    setCurrentPage(page);
+  };
+
+  const openDeleteModal = (id: string) => {
+    setProductToDelete(id);
+    setModalOpen(true);
+  }
+
+  const closeModal = () => {
+    setModalOpen(false);
+    setProductToDelete(null);
+  };
+
+  const confirmDelete = async () => {
+    if(!productToDelete) return;
+
+    try{
+      await deleteProduct(productToDelete);
+      setProducts(products.filter((product) => product.id !== productToDelete));
+      closeModal();
+    } catch (error) {
+      console.error('Error al eliminar el producto', error);
+    }
+  };
+
+
+
+  if(loading) return <div>Cargando...</div>;
+  if(products.length === 0) return <div>No hay productos disponibles.</div>
+  
+  return (
+    <div>
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="text-2xl font-bold text-red-500">Todos los productos</h2>
+        <Link href="/superadmin/products/create">
+        <button className="bg-red-500 text-white px-4 py-2 rounded">
+      Crear Producto
+      </button>
+      </Link>
+      </div>
+      <table className="w-full table-auto">
+        <thead>
+          <tr>
+            <th className="px-4 py-2">Nombre</th>
+            <th className="px-4 py-2">Categoría</th>
+            <th className="px-4 py-2">Precio</th>
+            <th className="px-4 py-2">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product)=>(
+            <tr key= {product.id}>
+              <td className="border px-4 py-2">{product.name}</td>
+              <td className="border px-4 py-2">
+                {Array.isArray(product.category)
+                ? product.category.map((cat:any) => cat.name).join(",")
+                : product.category}
+                </td>
+              <td className="border px-4 py-2">${product.price}</td>
+              <td className="border px-4 py-2">
+              <button className="bg-green-500 text-white px-2 py-1 rounded mr-2">
+                <Link href={`/superadmin/products/edit/${product.id.toString()}`}>
+                Editar
+                </Link>
+                </button>
+                <button
+                onClick={() => openDeleteModal(product.id)}
+                className="bg-red-500 text-white px-2 py-1 rounded"
+                >
+                  Eliminar
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="mt-4 flex justify-center items-center">
+        <button
+         onClick={() => handlePageChange(currentPage - 1)}
+         disabled= {currentPage === 1}
+         className="bg-gray-200 text-gray-600 px-3 py-1 rounded mr-2"
+         >
+          Anterior
+         </button>
+         <button
+         onClick={() => handlePageChange(currentPage + 1)}
+         disabled= {currentPage === totalPages}
+         className="bg-gray-200 text-gray-600 px-3 py-1 rounded mr-2"
+         >
+          Siguiente
+         </button>
+      </div>
+      <ConfirmModal
+       isOpen={modalOpen}
+       onConfirm={confirmDelete}
+       onCancel={closeModal}
+       title="Confirmar eliminación"
+       message="¿Estás seguro de que quieres eliminar este producto? Esta acción no se puede deshacer."
+       />
+    </div>
+  );
+};
+
+export default ProductList;
---
+++ src/helpers/peticionesSuperAdmin.tsx
@@ -0,0 +1,96 @@
+import { Product } from "@/types";
+
+const API_URL = 'https://chilyapi.onrender.com';
+
+const handleResponse = async (response: Response) => {
+  if(!response.ok) {
+    const error = await response.json();
+    throw new Error(error.message || 'Error en la solicitud');
+  }
+  return response.json();
+};
+
+export const fetchProducts = async (): Promise<Product[]> => {
+  const response = await fetch(`${API_URL}/products`);
+  if(!response.ok) {
+    throw new Error ('Error al obtener los productos');
+  }
+  const products: Product[] = await response.json();
+  return products;
+};
+
+export const createProduct = async (data: any) => {
+  try{
+    const response = await fetch(`${API_URL}/products/create`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    return handleResponse(response);
+  } catch (error) {
+    console.error('Error al crear el producto', error);
+    throw error;
+  }
+};
+
+export const updateProduct = async (id: string, data:any) => {
+  try {
+    const response = await fetch(`${API_URL}/products/update/${id}`,{
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    return handleResponse(response);
+  } catch(error) {
+    console.error('Error al actualizar el producto', error);
+    throw error;
+  }
+};
+
+export const deleteProduct = async (id: string) => {
+  try {
+    const response = await fetch(`${API_URL}/products/delete/${id}`, {
+      method: 'DELETE',
+    });
+    return handleResponse(response);
+  }catch (error) {
+    console.error('Error al eliminar el producto', error);
+    throw error;
+  }
+};
+
+export const PopularProduct = async (productId: string, status: boolean) => {
+  try{
+    const productIdNumber = parseInt(productId, 10)
+    const statusString = status ? 'true' : 'false';
+     const response = await fetch(`${API_URL}/products/popular?id=${productIdNumber}&status=${statusString}`, {
+     method: 'PUT',
+     headers: {
+      'Content-Type': 'application/json',
+    },
+    });
+    return handleResponse(response);
+  } catch(error) {
+    console.error('Error al marcar el producto como popular', error);
+    throw error;
+  }
+};
+
+export async function updateProductStock(productId: string, newStock:number) {
+  try{
+    const response = await fetch(`${API_URL}/products/stock?id=${productId}&stock=${newStock}`,{
+      method: 'PUT',
+      headers:{
+        'Content-Type': 'application/json',
+      },
+    });
+    return handleResponse(response);
+  } catch(error) {
+    console.error('Error en la petición', error);
+    throw error;
+  }
+}
