e35c5a2f6a302572918675543edff4556dc5c668
indisardi99 <indisardi99@gmail.com>
2024-07-05 16:31:37 -0300

    feat(reset password): reset password

--- src/app/req-reset/page.tsx
+++ src/app/req-reset/page.tsx
@@ -11,6 +11,7 @@ import {
 } from '@/app/components/ui/form';
 import { Input } from '@/app/components/ui/input';
 import { showToast } from '@/lib/utils';
+import { Button } from '../components/ui/button';
 
 const formSchema = z.object({
   email: z
@@ -27,42 +28,52 @@ function ReqResetpass() {
   });
 
   const onSubmit = async (data: z.infer<typeof formSchema>) => {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/auth/request-password-reset`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/request-password-reset`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
         },
-        body: JSON.stringify(data),
-      },
-    );
-    if (response.status === 201) {
-      const data = await response.json();
-      console.log(data);
-    } else {
-      showToast('error', <p>Hubo un problema al restablecer contraseña</p>);
+      );
+
+      if (response.status === 201) {
+        await response.json();
+        showToast('success', <p>Te enviamos un correo a tu email.</p>);
+      } else {
+        showToast('error', <p>Hubo un problema, intenta nuevamente</p>);
+      }
+    } catch (error) {
+      showToast('error', <p>Hubo un problema al restablecer la contraseña</p>);
     }
   };
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-[310px]">
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input placeholder="correo electrónico" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <button type="submit">Enviar</button>
-      </form>
-    </Form>
+    <div className="flex flex-col min-h-screen w-full h-full bg-red-500 items-center justify-start rounded-xl text-white font-semibold pt-[64px]">
+      <h1 className="text-[40px]">Donde chily</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-[310px]">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="correo electrónico" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" variant="submit" size="submit">
+            Recuperar contraseña
+          </Button>
+        </form>
+      </Form>
+    </div>
   );
 }
 
--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,18 +1,72 @@
 'use client';
 
 import { showToast } from '@/lib/utils';
-import { useSearchParams } from 'next/navigation';
-import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { Suspense, useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/app/components/ui/form';
+import { Input } from '@/app/components/ui/input';
+import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
+import { Button } from '../components/ui/button';
 
-function ResetPassword() {
+ function ResetPassword() {
   const searchParams = useSearchParams();
   const token = searchParams.get('token');
 
-  const [newPassword, setNewPassword] = useState('');
+  const formSchema = z
+    .object({
+      password: z
+        .string()
+        .min(8, {
+          message: 'La contraseña debe tener por lo menos 8 caracteres.',
+        })
+        .regex(/[a-z]/, {
+          message: 'La contraseña debe contener al menos una letra minúscula.',
+        })
+        .regex(/[A-Z]/, {
+          message: 'La contraseña debe contener al menos una letra mayúscula.',
+        })
+        .regex(/[0-9]/, {
+          message: 'La contraseña debe contener al menos un número.',
+        })
+        .regex(/[^a-zA-Z0-9]/, {
+          message: 'La contraseña debe contener al menos un carácter especial.',
+        }),
+      confirmPassword: z.string(),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: 'Las contraseñas deben coincidir.',
+      path: ['confirmPassword'],
+    });;
 
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
+   const form = useForm<z.infer<typeof formSchema>>({
+     resolver: zodResolver(formSchema),
+     defaultValues: {
+       password: '',
+     },
+   });
 
+   const { watch, trigger } = form;
+   const router = useRouter();
+   const [showPassword, setShowPassword] = useState(false);
+
+     useEffect(() => {
+       const subscription = watch(() => {
+         trigger();
+       });
+       return () => subscription.unsubscribe();
+     }, [watch, trigger]);
+
+
+  const handleSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       const response = await fetch(
         `${process.env.NEXT_PUBLIC_API_URL}/auth/reset-password`,
@@ -23,13 +77,13 @@ function ResetPassword() {
           },
           body: JSON.stringify({
             token: token,
-            newPassword: newPassword,
+            newPassword: values.password,
           }),
         },
       );
       if (response.ok) {
         showToast('success', <p>Contraseña restablecida con éxito.</p>);
-        // router.push('/login');
+        router.push('/login');
       } else {
         showToast('error', <p>Error al restablecer contraseña.</p>);
         // router.push('/register');
@@ -41,24 +95,70 @@ function ResetPassword() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Nueva contraseña:
-        <input
-          type="password"
-          value={newPassword}
-          onChange={(e) => setNewPassword(e.target.value)}
+   
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleSubmit)} className="w-[310px]">
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <div className="text-red-600 relative">
+                  <FormControl>
+                    <Input
+                      placeholder="Contraseña"
+                      {...field}
+                      type={showPassword ? 'text' : 'password'}
+                    />
+                  </FormControl>
+                  <div
+                    className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer"
+                    onClick={() => setShowPassword((prev) => !prev)}
+                  >
+                    {showPassword ? (
+                      <AiOutlineEyeInvisible />
+                    ) : (
+                      <AiOutlineEye />
+                    )}
+                  </div>
+                </div>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+           <FormField
+          control={form.control}
+          name="confirmPassword"
+          render={({ field }) => (
+            <FormItem>
+              <div className="text-red-600 relative">
+                <FormControl>
+                  <Input
+                    placeholder="Repetir contraseña"
+                    {...field}
+                    type={showPassword ? 'text' : 'password'}
+                  />
+                </FormControl>
+              </div>
+              <FormMessage />
+            </FormItem>
+          )}
         />
-      </label>
-      <button type="submit">Restablecer contraseña</button>
-    </form>
+          <Button type="submit" variant="submit" size="submit">
+            Recuperar contraseña
+          </Button>
+        </form>
+      </Form>
   );
 }
 
-export default function Auth() {
+export default function ResetPassPage(){
   return (
-    <div>
-      <ResetPassword />
-    </div>
+    <div className="flex flex-col min-h-screen w-full h-full bg-red-500 items-center justify-start rounded-xl text-white font-semibold pt-[64px]">
+      <h1 className="text-[40px]">Donde chily</h1>
+      <Suspense>
+        <ResetPassword />
+      </Suspense>
+      </div>
   );
 }
