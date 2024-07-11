58781862af94c3531b87070030c134db0a6ed247
indisardi99 <indisardi99@gmail.com>
2024-07-11 20:49:57 -0300

    merge conflict solved

--- src/app/components/AdminProfile/search.tsx
+++ src/app/components/AdminProfile/search.tsx
@@ -3,9 +3,13 @@ import { Button } from '../ui/button';
 
 interface SearchBarProps {
   onSearch: (query: string) => void;
+  searchValue?: string;
 }
 
-export const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+export const SearchBar: React.FC<SearchBarProps> = ({
+  onSearch,
+  searchValue,
+}) => {
   const [query, setQuery] = useState('');
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -17,20 +21,32 @@ export const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
   };
 
   return (
-    <div className="flex ">
+    <form
+      className="flex"
+      onSubmit={(e) => {
+        e.preventDefault;
+      }}
+    >
       <input
         type="text"
         placeholder="Buscar por email"
-        value={query}
+        value={query || searchValue}
         onChange={handleInputChange}
         className="border border-gray-300 rounded-md focus:outline-none "
       />
       <Button
+        type="submit"
         onClick={handleSearch}
         className="bg-red-500 text-white  rounded-md"
       >
         Buscar
       </Button>
-    </div>
+      <Button
+        onClick={handleSearch}
+        className="bg-red-500 text-white  rounded-md"
+      >
+        Reiniciar
+      </Button>
+    </form>
   );
 };
