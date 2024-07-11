172070031a82e56c03a76642a76c7e9a81659680
Esaul Alfacan <esaulalfacan@gmail.com>
2024-07-11 13:30:52 -0600

    feat: :sparkles: Update footer
    Add page to contact and privacity.

---
+++ src/app/(footer)/policy-privacy/page.tsx
@@ -0,0 +1,138 @@
+import React from 'react';
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="bg-gray-100 text-black flex flex-col w-full min-h-full mx-auto p-8">
+      <h1 className="text-4xl font-bold mb-8 text-center">
+        Política de Privacidad
+      </h1>
+      <p className="mb-6">
+        En Donde Chily, valoramos y respetamos su privacidad. Esta política de
+        privacidad describe cómo recopilamos, usamos y protegemos su información
+        personal cuando interactúa con nuestro sitio web. Nos comprometemos a
+        garantizar que su información personal esté protegida y que se utilice
+        de acuerdo con esta política.
+      </p>
+
+      <h2 className="text-3xl font-semibold mb-4">
+        1. Información que Recopilamos
+      </h2>
+      <p className="mb-6">
+        Podemos recopilar información personal que usted nos proporciona
+        directamente, como su nombre, dirección de correo electrónico, número de
+        teléfono y cualquier otra información que decida compartir con nosotros.
+        Además, podemos recopilar información automáticamente a través del uso
+        de cookies y otras tecnologías de seguimiento, como su dirección IP,
+        tipo de navegador, páginas visitadas y tiempo de permanencia en nuestro
+        sitio.
+      </p>
+
+      <h2 className="text-3xl font-semibold mb-4">2. Uso de la Información</h2>
+      <p className="mb-6">
+        La información que recopilamos se utiliza para los siguientes fines:
+      </p>
+      <ul className="list-disc list-inside mb-6">
+        <li>Proporcionarle nuestros servicios y productos.</li>
+        <li>
+          Mejorar la funcionalidad y la experiencia del usuario en nuestro sitio
+          web.
+        </li>
+        <li>
+          Comunicarnos con usted para responder a sus consultas y solicitudes.
+        </li>
+        <li>
+          Enviar boletines informativos y promociones, si ha optado por
+          recibirlos.
+        </li>
+        <li>Cumplir con nuestras obligaciones legales y reglamentarias.</li>
+      </ul>
+
+      <h2 className="text-3xl font-semibold mb-4">3. Compartir Información</h2>
+      <p className="mb-6">
+        No compartimos su información personal con terceros, excepto en las
+        siguientes circunstancias:
+      </p>
+      <ul className="list-disc list-inside mb-6">
+        <li>
+          Cuando es necesario para proporcionarle nuestros servicios, como con
+          proveedores de servicios y socios comerciales.
+        </li>
+        <li>
+          Para cumplir con obligaciones legales, como responder a solicitudes
+          legales y prevenir fraudes.
+        </li>
+        <li>
+          Para proteger nuestros derechos y la seguridad de nuestros usuarios.
+        </li>
+      </ul>
+
+      <h2 className="text-3xl font-semibold mb-4">
+        4. Seguridad de la Información
+      </h2>
+      <p className="mb-6">
+        Implementamos medidas de seguridad razonables para proteger su
+        información personal contra accesos no autorizados, alteraciones,
+        divulgación o destrucción. Sin embargo, ningún método de transmisión por
+        Internet o método de almacenamiento electrónico es completamente seguro,
+        por lo que no podemos garantizar la seguridad absoluta de su
+        información.
+      </p>
+
+      <h2 className="text-3xl font-semibold mb-4">5. Sus Derechos</h2>
+      <p className="mb-6">
+        Usted tiene el derecho de acceder, corregir o eliminar su información
+        personal. También tiene el derecho de retirar su consentimiento para el
+        procesamiento de sus datos en cualquier momento. Para ejercer estos
+        derechos, puede contactarnos en privacidad@dondechily.com.
+      </p>
+
+      <h2 className="text-3xl font-semibold mb-4">6. Uso de Cookies</h2>
+      <p className="mb-6">
+        Utilizamos cookies y tecnologías similares para mejorar su experiencia
+        en nuestro sitio web. Las cookies son pequeños archivos de datos que se
+        almacenan en su dispositivo cuando visita nuestro sitio. Puede
+        configurar su navegador para que rechace las cookies o le avise cuando
+        se envíen cookies. Sin embargo, si desactiva las cookies, es posible que
+        algunas funciones de nuestro sitio web no funcionen correctamente.
+      </p>
+
+      <h2 className="text-3xl font-semibold mb-4">
+        7. Enlaces a Otros Sitios Web
+      </h2>
+      <p className="mb-6">
+        Nuestro sitio web puede contener enlaces a otros sitios web de interés.
+        Sin embargo, una vez que haya utilizado estos enlaces para salir de
+        nuestro sitio, debe tener en cuenta que no tenemos control sobre esos
+        otros sitios web. Por lo tanto, no podemos responsabilizarnos por la
+        protección y privacidad de cualquier información que proporcione
+        mientras visita dichos sitios, ya que no están sujetos a esta política
+        de privacidad.
+      </p>
+
+      <h2 className="text-3xl font-semibold mb-4">
+        8. Cambios a esta Política
+      </h2>
+      <p className="mb-6">
+        Nos reservamos el derecho de actualizar esta política de privacidad en
+        cualquier momento. Le notificaremos sobre cualquier cambio publicando la
+        nueva política en nuestro sitio web. Le recomendamos que revise
+        periódicamente esta página para asegurarse de que está de acuerdo con
+        los cambios.
+      </p>
+
+      <h2 className="text-3xl font-semibold mb-4">9. Contacto</h2>
+      <p className="mb-6">
+        Si tiene alguna pregunta sobre nuestra política de privacidad o sobre
+        cómo manejamos su información personal, puede contactarnos en
+        privacidad@dondechily.com.
+      </p>
+
+      <p className="mb-6">
+        Esta política de privacidad se actualizó por última vez el 10 de julio
+        de 2024.
+      </p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
